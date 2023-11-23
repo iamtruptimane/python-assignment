f0483032fe49dd3ca0c6f5141def5e61c6672d17
Your Name <you@example.com>
2023-11-23 12:03:11 +0530

    day 17 of 100dayschallange

---
+++ leetcode/string/17-final-value-of-var.py
@@ -0,0 +1,17 @@
+def finalValueAfterOperations(operations):
+        x = 0
+        
+        for operation in operations:
+            if operation == "X++" or operation == "++X":
+                x = x + 1
+            if operation == "--X" or operation == "X--":
+                x = x - 1
+        return x  
+operations = ["++X","++X","X++"]
+print(finalValueAfterOperations(operations))              
+            
+
+
+        
+
+        
