6866f6fa531ec07b0329a4f992f627ea3ddc1c9c
Your Name <you@example.com>
2023-11-24 09:10:57 +0530

    added folder of practice problems of list

---
+++ practice_problem_list/01-reverse-list.py
@@ -0,0 +1,33 @@
+def reverse_list():
+    
+    list_1 = [100, 200, 300, 400, 500]
+    
+    list_1.reverse()
+    
+    print(list_1)
+#reverse_list()
+
+def reverse_list1():
+     list_1 = [100, 200, 300, 400, 500]
+     
+     list_2 = []
+     
+     for num in list_1[::-1]:
+         list_2.append(num)
+     print(list_2)
+    
+reverse_list1()   
+
+leftsum = 0
+        start = 0
+        arr=[]
+
+        for num in nums[start:3]:
+            if 
+            leftsum = leftsum + num
+            print(leftsum)
+            arr.append(leftsum)
+        return arr         
+        
+    
+           
